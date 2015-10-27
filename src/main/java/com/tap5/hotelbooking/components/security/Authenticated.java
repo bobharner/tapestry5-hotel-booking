5584e85a8c3e3324412b36de5519a00bf2766a9e
Bob Harner <bobharner@gmail.com>
2015-10-26 21:37:29 -0400

    Added "Log in" link to nav menu

--- src/main/java/com/tap5/hotelbooking/components/security/Authenticated.java
+++ src/main/java/com/tap5/hotelbooking/components/security/Authenticated.java
@@ -6,7 +6,9 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 import com.tap5.hotelbooking.services.Authenticator;
 
 /**
- * Checks if the user is logged in
+ * Checks if the user is logged in, and only renders its body if the user
+ * is logged in. Like the <em>if</em> component, an optional "else" parameter is
+ * supported for a block to render when the user is <em>not</em> logged in.
  * 
  * @author karesti
  */
