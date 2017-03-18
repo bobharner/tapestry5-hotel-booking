e70d3d9cf927829dd689f27adf6c4e010d785aea
Bob Harner <bobharner@gmail.com>
2017-03-18 17:58:21 -0400

    Changed to use async=true approach instead of zone="..."

--- src/main/java/com/tap5/hotelbooking/pages/Index.java
+++ src/main/java/com/tap5/hotelbooking/pages/Index.java
@@ -1,14 +1,16 @@
 package com.tap5.hotelbooking.pages;
 
+import org.apache.tapestry5.Block;
 import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.annotations.SessionState;
-import org.apache.tapestry5.corelib.components.Zone;
+import org.apache.tapestry5.corelib.components.Grid;
 import org.apache.tapestry5.grid.GridDataSource;
 import org.apache.tapestry5.hibernate.HibernateGridDataSource;
 import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.ajax.AjaxResponseRenderer;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Criterion;
@@ -29,8 +31,14 @@ public class Index
     @Inject
     private Session session;
 
+    @Inject
+    private AjaxResponseRenderer ajaxResponseRenderer;
+
     @InjectComponent
-    private Zone result;
+    private Grid resultTable;
+
+    @Inject
+    private Block resultsArea;
 
     @SessionState
     @Property
@@ -78,14 +86,10 @@ public class Index
      * TODO: Move this into a hotel query service
      */
     @OnEvent(value = EventConstants.SUCCESS)
-    Object searchHotels()
+    void searchHotels()
     {
-        return result.getBody();
-    }
-    
-    Object onAction()
-    {
-        return result.getBody();
+        // tell Tapestry to render the "resultArea" zone
+        ajaxResponseRenderer.addRender("resultArea", resultTable);
     }
 
 }
